4c58bca57b1e24de99b0e1f3ec59647161d8842b
Gagego Maciel <gagego.maciel@gmail.com>
2021-09-07 10:06:35 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import { DataTable } from "components/DataTable";
+import { Footer } from "components/Footer";
+import { NavBar } from "components/NavBar";
 
 
 function App() {
   return (
-   <h1 className="text-primary">Hello World</h1>
+    <>
+      <NavBar />
+      <div className="container">
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
