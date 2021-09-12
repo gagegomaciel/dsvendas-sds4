40c9e07ff97aebbf264aec37640e094b2d27dcb7
Gagego Maciel <gagego.maciel@gmail.com>
2021-09-12 18:05:43 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,6 +1,30 @@
+import { useState } from 'react';
 import Chart from 'react-apexcharts';
 
+import api from '../../services/api';
+import { SaleSum} from '../../types/sale';
+
+interface IChartDataProps {
+  labels: string[];
+  series: number[];
+}
+
+
+
 export const DonutChat = () => {
+  let charData : IChartDataProps = { labels: [], series: [] }
+  api.get('/sales/amount-by-seller').then((response) => {
+    const data = response.data as SaleSum[];
+    const myLabels = data.map(item => item.sellerName);
+    const mySeries = data.map(item => item.sum);
+
+    charData = { labels: myLabels, series: mySeries };
+
+    console.log(charData);
+
+  });
+
+
   const mockData = {
     series: [477138, 499928, 444867, 220426, 473088],
     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
