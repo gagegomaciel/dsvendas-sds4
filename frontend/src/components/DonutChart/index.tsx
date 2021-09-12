90239ce3106e5fbfbf6b0fe197440bf751f7b8b9
Gagego Maciel <gagego.maciel@gmail.com>
2021-09-12 18:19:39 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 
 import api from '../../services/api';
 import { SaleSum} from '../../types/sale';
-
 interface IChartDataProps {
   labels: string[];
   series: number[];
 }
 
-
-
 export const DonutChat = () => {
-  let charData : IChartDataProps = { labels: [], series: [] }
-  api.get('/sales/amount-by-seller').then((response) => {
-    const data = response.data as SaleSum[];
-    const myLabels = data.map(item => item.sellerName);
-    const mySeries = data.map(item => item.sum);
+  const [chartData, setChartData] = useState<IChartDataProps>({ labels: [], series: [] });
 
-    charData = { labels: myLabels, series: mySeries };
-
-    console.log(charData);
-
-  });
-
-
-  const mockData = {
-    series: [477138, 499928, 444867, 220426, 473088],
-    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-  }
+  useEffect(() => {
+    api.get('/sales/amount-by-seller').then((response) => {
+      const data = response.data as SaleSum[];
+      const myLabels = data.map(item => item.sellerName);
+      const mySeries = data.map(item => item.sum);
+      
+  
+      setChartData({ labels: myLabels, series: mySeries });
+  
+      console.log(chartData);
+  
+    });
+  }, [])
+ 
 
   const options = {
       legend: {
@@ -37,8 +33,8 @@ export const DonutChat = () => {
   }
   return (
     <Chart 
-      options={{ ...options, labels: mockData.labels }}
-      series={mockData.series}
+      options={{ ...options, labels: chartData.labels }}
+      series={chartData.series}
       type="donut"
       height="240"
     />
